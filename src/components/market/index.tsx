42b3dd24670d5fcf46320f84a602f827e2d86686
guozr <guozr1997@hotmail.com>
2022-05-15 09:51:34 +0800

    update

--- src/components/market/index.tsx
+++ src/components/market/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import {Space} from 'antd';
+import {Row, Space} from 'antd';
 import TradeCard from "../info_card/trade_card";
 
 export default function Market() {
     return (
-        <div>
+        <Row>
+            <Space>
+                <TradeCard/>
+            </Space>
             <Space size={[8, 16]} wrap>
                 {new Array(10).fill(null).map((_, index) => (
                     // eslint-disable-next-line react/no-array-index-key
                     <TradeCard/>
                 ))}
             </Space>
-        </div>
+        </Row>
+
     )
 }
